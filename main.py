2623efe8098786e2ec6c79e97ed36a9fc13eb69c
Dima-aka-dima <99353558+Dima-aka-dima@users.noreply.github.com>
2023-12-30 18:10:23 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,116 @@
+import numpy as np
+
+def getNamedNumpyClass(className, variableNames):
+    
+    namedClass = type(className, (np.ndarray,), {
+        'dict': dict(zip(variableNames, range(len(variableNames))))})
+    
+    # creation from np.ndarray
+    def _new(self, array):
+
+        assert (len(self.dict), ) == array.shape, f"Shape should be {(len(self.dict), )}"
+        return array.view(self)
+
+    # update array on setting variables
+    def _setattr(self, name, value):
+        if name in self.dict: self[self.dict[name]] = value
+        else: self[name] = value
+      
+    # addressing variables as attributes
+    def _getattr(self, name):
+        if name in self.dict: name = self.dict[name]
+        return self[name]
+        
+    setattr(namedClass, '__new__', _new)
+    setattr(namedClass, '__setattr__', _setattr)
+    setattr(namedClass, '__getattr__', _getattr)
+        
+    return namedClass
+
+
+def getNamedNumpyArrayClass(className, variableNames):
+    
+    namedClass = type(className, (np.ndarray,), {
+        'dict': dict(zip(variableNames, range(len(variableNames))))})
+    
+    # creation from np.ndarray
+    def _new(self, array):
+        assert len(self.dict) == array.shape[-1], f"Last dimension should be {len(self.dict)}."
+        return array.view(self)
+
+    # update array on setting variables
+    def _setattr(self, name, value):
+        if name in self.dict: self[...,self.dict[name]] = value
+        else: self[name] = value
+      
+    # addressing variables as attributes
+    def _getattr(self, name):
+        if name in self.dict: return self[...,self.dict[name]]
+        else: return self[name]
+        
+    setattr(namedClass, '__new__', _new)
+    setattr(namedClass, '__setattr__', _setattr)
+    setattr(namedClass, '__getattr__', _getattr)
+        
+    return namedClass
+
+
+## This is typed version but mypy doesn't like it
+
+# from typing import List, Any
+# from numpy.typing import NDArray, DTypeLike
+
+# def getNamedNumpyClass(className: str, variableNames: List[str], T: DTypeLike = np.generic):
+#     
+#     namedClass = type(className, (np.ndarray,), {
+#         'dict': dict(zip(variableNames, range(len(variableNames))))})
+#     
+#     # creation from np.ndarray
+#     def _new(self, array: NDArray[T]):
+# 
+#         assert (len(self.dict), ) == array.shape, f"Shape should be {(len(self.dict), )}"
+#         return array.view(self)
+# 
+#     # update array on setting variables
+#     def _setattr(self, name, value):
+#         if name in self.dict: self[self.dict[name]] = value
+#         else: self[name] = value
+#       
+#     # addressing variables as attributes
+#     def _getattr(self, name):
+#         if name in self.dict: name = self.dict[name]
+#         return self[name]
+#         
+#     setattr(namedClass, '__new__', _new)
+#     setattr(namedClass, '__setattr__', _setattr)
+#     setattr(namedClass, '__getattr__', _getattr)
+#         
+#     return namedClass
+# 
+# 
+# def getNamedNumpyArrayClass(className: str, variableNames: List[str], T: DTypeLike = np.generic):
+#     
+#     namedClass = type(className, (np.ndarray,), {
+#         'dict': dict(zip(variableNames, range(len(variableNames))))})
+#     
+#     # creation from np.ndarray
+#     def _new(self, array: NDArray[T]):
+#         assert len(self.dict) == array.shape[-1], f"Last dimension should be {len(self.dict)}."
+#         return array.view(self)
+# 
+#     # update array on setting variables
+#     def _setattr(self, name, value):
+#         if name in self.dict: self[...,self.dict[name]] = value
+#         else: self[name] = value
+#       
+#     # addressing variables as attributes
+#     def _getattr(self, name):
+#         if name in self.dict: return self[...,self.dict[name]]
+#         else: return self[name]
+#         
+#     setattr(namedClass, '__new__', _new)
+#     setattr(namedClass, '__setattr__', _setattr)
+#     setattr(namedClass, '__getattr__', _getattr)
+#         
+#     return namedClass
+# 
